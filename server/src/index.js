53de1f1885a42b52a83a604c82f121bbf5cf17b9
GeriCookie <gergana.bailova@gmail.com>
2019-08-19 12:39:03 +0300

    Added countries-list and basic implementation of the resolvers

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,56 @@
+import cors from 'cors'
 import express from 'express'
 import { ApolloServer, gql } from 'apollo-server-express'
 
+import { continents, countries, languages } from 'countries-list'
+
 const app = express()
+
+app.use(cors())
+
+const getCountries = () => {
+  const countriesArr = Object.entries(countries)
+  const countriesList = []
+  for (const [code, countryObj] of countriesArr) {
+    const continent = {
+      code: countryObj.continent,
+      name: continents[countryObj.continent]
+    }
+    const languagesArr = []
+    countryObj.languages.forEach(languageCode => languagesArr.push(languages[languageCode].name))
+    const country = {
+      code: code,
+      name: countryObj.name,
+      native: countryObj.native,
+      phone: countryObj.phone,
+      continent: continent,
+      capital: countryObj.capital,
+      currency: countryObj.currency,
+      languages: languagesArr,
+      emoji: countryObj.emoji,
+      emojiU: countryObj.emojiU
+    }
+    countriesList.push(country)
+  }
+  return countriesList
+}
+
+ const getContinents = () => {
+  const continentsArr = Object.entries(continents)
+  const continentsList = []
+  const countriesArray = getCountries()
+  for (const [code, continentName] of continentsArr) {
+    const countriesList = countriesArray.filter(country => country.continent.code === code)
+    const continent = {
+      code: code,
+      name: continentName,
+      countries: countriesList
+    }
+
+    continentsList.push(continent)
+  }
+  return continentsList
+}
 
 const schema = gql`
   type Query {
@@ -32,12 +81,37 @@ const schema = gql`
 
 const resolvers = {
   Query: {
-    country: () => {
-      return {
-        "name": "test"
-      }
+    continents: () => {
+      const continents = getContinents()
+      return continents
+    },
+    continent: (parent, { code }) => {
+      const continents = getContinents()
+      return continents.find(continent => continent.code === code)
+    },
+    countries: () => {
+      const countries = getCountries()
+      return countries
+    },
+    country: (parent, { code }) => {
+      const countries = getCountries()
+      return countries.find(country => country.code === code)
     }
-  }
+  },
+
+  Country: {
+    continent: country => {
+      const continents = getContinents()
+      return continents.find(continent => continent.code === country.continent.code)
+    }
+  },
+
+  Continent: {
+    countries: continent => {
+      const countries = getCountries()
+      return countries.filter(country => country.continent.code === continent.code)
+    }
+  } 
 }
 
 const server = new ApolloServer({
